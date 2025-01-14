8a3b9159404d5db9745ddc2473711d1ed594d259
Mostafa Darehzereshki <mostafa69d@gmail.com>
2025-01-14 17:11:31 -0500

    adds pending state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { formSubmitAction } from "./actions";
 import { useState } from "react";
+import ReactMarkdown from 'react-markdown'
 
 export default function Home() {  
   const [suggestions, setSuggestions] = useState<string | null | undefined>();
+  const [isPending, setIsPending] = useState<boolean>(false);
+
   async function handleUserRequestFormSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    setIsPending(true);
     const formData = new FormData(event.currentTarget);
     const response = await formSubmitAction(formData);
     setSuggestions(response);
+    setIsPending(false);
   }
   return (
-      <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-300 w-screen h-screen">
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-300 w-screen px-10">
       <form
         className="bg-white shadow-lg rounded-lg p-6 w-full max-w-lg"
         onSubmit={handleUserRequestFormSubmit}
@@ -29,13 +34,15 @@ export default function Home() {
             required
           />
         </div>
-        <button
+
+         <button
           type="submit"
-          className="w-full px-4 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 transition duration-300 disabled:opacity-50"
+          className={`w-full px-4 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 transition duration-300 disabled:opacity-50 flex justify-center ${isPending ? 'animate-pulse' : ''}`}
+          disabled={isPending}
         >
           Submit
         </button>
-        <div>{suggestions}</div>
+        <ReactMarkdown className="text-black mt-4">{suggestions}</ReactMarkdown>
       </form>
     </div>
   );
